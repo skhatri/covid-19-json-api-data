cfe9218c33bc577945636726dfd75b4aa04aca8a
Suresh Khatri <servlet2@msn.com>
2020-04-13 11:04:49 +1000

    gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,3 @@
-import org.gradle.api.artifacts.maven.MavenDeployment
-import org.gradle.api.tasks.bundling.Jar
-
-buildscript {
-    fun createBuildVersion(projectVersion: String): String {
-        var derivedVersion = projectVersion
-        val versionWithSnapshot = projectVersion.replace("-SNAPSHOT", "")
-        val buildNumber = System.getenv("TRAVIS_BUILD_NUMBER") ?: "0"
-        if (project.extra["release"] == "true") {
-            derivedVersion = "${versionWithSnapshot}.${buildNumber}"
-        } else {
-            derivedVersion = "${versionWithSnapshot}.${buildNumber}-SNAPSHOT"
-        }
-        println("effective project version: ${derivedVersion}")
-        return derivedVersion
-    }
-    project.version = createBuildVersion("${project.version}")
-}
-
 plugins {
     idea
     java
@@ -32,29 +13,22 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
-configurations {
-    implementation {
-        resolutionStrategy.failOnVersionConflict()
-    }
-}
+val sparkV by extra("3.0.0-preview2")
+val scalaV by extra("2.12.10")
 
 dependencies {
-    implementation ("org.scala-lang:scala-library:2.12.10")
-    implementation ("org.scala-lang:scala-compiler:2.12.10")
-    implementation ("org.apache.spark:spark-core_2.12:3.0.0-preview2")
-    implementation ("org.apache.spark:spark-sql_2.12:3.0.0-preview2")
-    implementation ("org.apache.spark:spark-csv_2.12:3.0.0-preview2")
-
-    testImplementation("org.mockito:mockito-core:3.3.3")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.1")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.6.1")
-
-    testImplementation("org.junit.platform:junit-platform-commons:1.6.1")
-    testImplementation("org.junit.platform:junit-platform-runner:1.6.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.6.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.6.1")
+    implementation("org.scala-lang:scala-library:$scalaV")
+    implementation("org.scala-lang:scala-compiler:$scalaV")
+    implementation("org.apache.spark:spark-core_2.12:$sparkV")
+    implementation("org.apache.spark:spark-sql_2.12:$sparkV")
 }
 
+task("extract", JavaExec::class) {
+    main = project.extra["app"] as String
+    classpath = sourceSets["main"].runtimeClasspath
+    jvmArgs = listOf("-Xms512m", "-Xmx512m")
+}
+
+tasks.wrapper {
+    gradleVersion = "6.2.2"
+}
