5d28285217ac45613b1a3315a3edfd6cefc63696
Suresh Khatri <servlet2@msn.com>
2020-04-13 12:28:41 +1000

    print summary

--- upload.sh
+++ upload.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+: ${TRAVIS_BUILD_NUMBER="0"}
+: ${TRAVIS_EVENT_TYPE="trigger"}
 pwd
 ls -lhtr
-git status
+git status -s
+now=$(date "+%Y-%m-%d %H:%M:%S%z")
 changes=$(git status -s|wc -l)
+echo "Time: ${now}, changes: ${changes}"
 if [[ $changes -ne 0 ]];
 then
-  git config --global user.email "travis@travis-ci.com"
-  git config --global user.name "Travis CI"
+  email=$(git config user.email)
+
+  ec=$(git config user.email|wc -l)
+  if [[ $ec -eq 0 ]];
+  then
+      git config --global user.email "travis@travis-ci.com"
+  fi;
+
+  nc=$(git config user.name|wc -l)
+  if [[ $nc -eq 0 ]];
+  then
+    git config --global user.name "Travis CI"
+  fi;
 
   git add data
   git add dataset
-  now=$(date "+%Y-%m-%d %H:%M:%S%z")
-  git commit -m"dataset update run at ${now} build: #${TRAVIS_BUILD_NUMBER}, trigger: ${TRAVIS_EVENT_TYPE}"
+  git commit -m"dataset update run at ${now} build: #${TRAVIS_BUILD_NUMBER}, trigger_type: ${TRAVIS_EVENT_TYPE}"
   git push origin master
 fi
