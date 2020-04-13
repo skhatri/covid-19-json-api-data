1673e1134ac7aec0d22a2419b6005dbbd74819b4
Suresh Khatri <servlet2@msn.com>
2020-04-13 16:44:52 +1000

    only build develop branch

--- upload.sh
+++ upload.sh
@@ -30,7 +30,9 @@ then
   git remote add gh https://"${GITHUB_TOKEN}"@"${GIT_URL}"
   tag_msg='{"time": "'"${now}"'", "build_number": '"${TRAVIS_BUILD_NUMBER}"', "trigger_type": "'"${TRAVIS_EVENT_TYPE}"'", "message": "dataset update"}'
   git tag -a -m"${tag_msg}" build/"${TRAVIS_BUILD_NUMBER}"
-  git push gh build/"${TRAVIS_BUILD_NUMBER}":master
+  current=$(git branch --show-current)
+  echo "current branch ${current}"
+  git push gh "${current}":master
   git push gh --tags
 fi
 
