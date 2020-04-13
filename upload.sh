84c742ccf7b1b580930a73f15ba26dbee032c2f1
Suresh Khatri <servlet2@msn.com>
2020-04-13 11:16:21 +1000

    upload script

---
+++ upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+changes=$(git status -s|wc -l)
+if [[ $changes -ne 0 ]];
+then
+  git config --global user.email "travis@travis-ci.com"
+  git config --global user.name "Travis CI"
+
+  git add data
+  git add dataset
+  now=$(date "+%Y-%m-%d %H:%M:%S%z")
+  git commit -m"dataset update run at ${now} build: #${TRAVIS_BUILD_NUMBER}"
+  git push origin master
+fi
